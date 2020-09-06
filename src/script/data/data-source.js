fbd2339bed5ef7a9c29761d6c2362fbf7a4607d1
M Fachri <mfachri@upi.edu>
2020-09-06 17:23:37 +0700

    Arrow Function

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -4,9 +4,7 @@ function DataSource(onSuccess, onFailed) {
 }
 
 DataSource.prototype.searchFood = function (keyword) {
-    const filteredFoods = foods.filter(function (food) {
-       return food.name.toUpperCase().includes(keyword.toUpperCase());
-    });
+    const filteredFoods = foods.filter(food => food.name.toUpperCase().includes(keyword.toUpperCase()));
 
     if (filteredFoods.length) {
         this.onSuccess(filteredFoods);
