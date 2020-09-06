abda33ce189d047d2b46da48cb948edde1ef90c4
M Fachri <mfachri@upi.edu>
2020-09-06 20:13:21 +0700

    Web Component update

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import "./src/script/component/ideas-container.js";
+import "./src/script/component/navbar.js";
 import main from "./src/script/view/main.js";
 
 document.addEventListener("DOMContentLoaded", main);
