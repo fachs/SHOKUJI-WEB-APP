2548648c44f284cca9a9d3e32818c44eed511c46
M Fachri <mfachri@upi.edu>
2020-09-06 20:15:01 +0700

    Web Component update

---
+++ src/script/component/ideas-container.js
@@ -0,0 +1,35 @@
+class IdeasContainer extends HTMLElement {
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `
+        <h4>Inpiration and Ideas</h4>
+        <div class="thumbnail">
+                <div class="jumbo-thumbnail">
+                    <img src="src/assets/img/emy-XoByiBymX20-unsplash.jpg" alt="">
+                    <div class="overlay">Malaysian Spicy Curry</div>
+                </div>
+                <div class="img1 thumb">
+                    <img src="src/assets/img/homemade italian pizza.jpg" alt="">
+                    <div class="overlay-thumbnail">Home Made Italian Pizza</div>
+                </div>
+                <div class="img2 thumb">
+                    <img src="src/assets/img/Oreo Cookie sandwiches.jpg" alt="">
+                    <div class="overlay-thumbnail">Oreo Cookie Sandwiches</div>
+                </div>
+                <div class="img3 thumb">
+                    <img src="src/assets/img/slideshow/slide2.jpg" alt="">
+                    <div class="overlay-thumbnail bottom-thumb">Oreo Cookie Sandwiches</div>
+                </div>
+                <div class="img4 thumb">
+                    <img src="src/assets/img/slideshow/slide3.jpg" alt="" >
+                    <div class="overlay-thumbnail bottom-thumb">Chocolate Pancake</div>
+                </div>
+            </div>`;
+    }
+
+}
+
+customElements.define("ideas-container", IdeasContainer);
---
+++ src/script/component/navbar.js
@@ -0,0 +1,43 @@
+class NavBar extends HTMLElement {
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `<div class="dropdown">
+        <button class="dropbtn">Find by
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+            <div class="row">
+                <div class="column">
+                    <h3>Ingredients</h3>
+                    <a href="#">Ingredients 1</a>
+                    <a href="#">Ingredients 2</a>
+                    <a href="#">Ingredients 3</a>
+                    <a href="#">Ingredients 4</a>
+                </div>
+                <div class="column">
+                    <h3>Area</h3>
+                    <a href="#">Area 1</a>
+                    <a href="#">Area 2</a>
+                    <a href="#">Area 3</a>
+                </div>
+                <div class="column">
+                    <h3>Category</h3>
+                    <a href="#">Category 1</a>
+                    <a href="#">Category 2</a>
+                    <a href="#">Category 3</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <img src="src/assets/img/shokuji.png" alt="">
+    <div class="search-container" id="search-container">
+        <input type="search" name="search" id="searchElement" placeholder="Keyword...">
+        <button id="searchButtonElement" type="submit">Search</button>
+    </div>`;
+    }
+}
+
+customElements.define("nav-bar", NavBar);
