da39a7540f0a24af7f381a5b2830a4cc0890b7b7
M Fachri <mfachri@upi.edu>
2020-09-06 17:27:04 +0700

    Function to Class

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -3,14 +3,14 @@ class DataSource {
         this.onSuccess = onSuccess;
         this.onFailed = onFailed;
     }
-}
 
-DataSource.prototype.searchFood = function (keyword) {
-    const filteredFoods = foods.filter(food => food.name.toUpperCase().includes(keyword.toUpperCase()));
+    searchFood(keyword) {
+        const filteredFoods = foods.filter(food => food.name.toUpperCase().includes(keyword.toUpperCase()));
 
-    if (filteredFoods.length) {
-        this.onSuccess(filteredFoods);
-    } else {
-        this.onFailed(`${keyword} is not found`);
+        if (filteredFoods.length) {
+            this.onSuccess(filteredFoods);
+        } else {
+            this.onFailed(`${keyword} is not found`);
+        }
     }
-};
+}
