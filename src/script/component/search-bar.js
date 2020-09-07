b6b0d9a8d8c002310e25b3837e4ba9a1570faa1d
M Fachri <mfachri@upi.edu>
2020-09-07 09:54:43 +0700

    Shadow DOM implemented

--- src/script/component/search-bar.js
+++ src/script/component/search-bar.js
@@ -1,4 +1,10 @@
 class SearchBar extends HTMLElement {
+
+    constructor() {
+        super();
+        this.shadowDOM = this.attachShadow({mode: "open"});
+    }
+
     connectedCallback() {
         this.render();
     }
@@ -9,17 +15,54 @@ class SearchBar extends HTMLElement {
     }
 
     get value() {
-        return this.querySelector("#searchElement").value;
+        return this.shadowDOM.querySelector("#searchElement").value;
     }
 
     render() {
-        this.innerHTML = `
+        this.shadowDOM.innerHTML = `
+        <style>
+        .search-container {
+            float: right;
+            margin-top: 8px;
+            margin-right: 50px;
+            }
+            
+            .search-container input {
+            border: none;
+            background: #c4c1c1;
+            width: 650px;
+            height: 34px;
+            margin-top: 8px;
+            font-size: 17px;
+            padding-left: 16px;
+            }
+            
+            .search-container button {
+            float: right;
+            height: 36px;
+            width: 155px;
+            padding: 6px;
+            margin-top: 8px;
+            margin-left: 8px;
+            margin-right: 16px;
+            background: #B7F667;
+            border: none;
+            cursor: pointer;
+            font-size: 17px;
+            color: #474646;
+            }
+            
+            .search-container button:hover,  .link > a:hover{
+            background: #88bd42;
+            color: white;
+            }
+        </style>
         <div class="search-container" id="search-container">
             <input type="search" name="search" id="searchElement" placeholder="Keyword...">
             <button id="searchButtonElement" type="submit">Search</button>
         </div>`;
 
-        this.querySelector("#searchButtonElement").addEventListener("click", this._clickEvent);
+        this.shadowDOM.querySelector("#searchButtonElement").addEventListener("click", this._clickEvent);
     }
 }
 
